2659b3466b9589db631f7ff6f3b92082d07a7259
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-05-06 15:46:29 +0100

    CTE version of solution

--- sql/607-sales-person.sql
+++ sql/607-sales-person.sql
@@ -55,7 +55,28 @@ WHERE sales_id NOT IN (SELECT o.sales_id
     ON o.com_id = c.com_id
     WHERE o.com_id = (SELECT com_id FROM Company WHERE name = "RED"));
 
+/* same solution but faster since it doesn't use 2nd subquery */
+
+SELECT name
+FROM Salesperson
+WHERE sales_id NOT IN (SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c
+    ON o.com_id = c.com_id
+    WHERE c.name = "RED");
+
 /* MY SOLUTION - sub optimal, uses 2 subqueries within the main query, but premise goes:
 
 - 1) Join the orders table and company table together, joining only where the company name is "RED", as so to select only orders which are from the company we don't want and the sales_ids associated.
 - 2) Then, we can specify we only want the sales_id of those orders where the company name is red, and then put the result of 1) into a subquery, selecting only the name from the Salesperson table where the sales_id is not contained within the results of 1), which house the sales_ids of the salespeople who have done a deal with the company named "RED". */
+
+WITH t1 AS (SELECT sales_id
+    FROM Orders o
+    JOIN Company c
+    ON o.com_id = c.com_id
+    WHERE c.name = "RED")
+SELECT name
+FROM Salesperson
+WHERE sales_id NOT IN (SELECT sales_id FROM t1);
+
+/* MORE OPTIMAL - same logic as the subquery version, but places it into a WITH statement to make it easier, note you have to use a subquery to select from the WITH statement if you don't explicitly query from it in the FROM clause */
