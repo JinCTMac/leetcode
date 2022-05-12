aa6426f8b7d42c6b23eca26da95a7647eb564f4d
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-05-12 14:37:55 +0100

    1148 solution

--- sql/1148-article-views-i.sql
+++ sql/1148-article-views-i.sql
@@ -1,1 +1,27 @@
 -- 1148 - Article Views I
+
+/* Table: Views
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date.
+Note that equal author_id and viewer_id indicate the same person.
+
+
+Write an SQL query to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order. */
+
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id;
+
+/* SOLUTION - again simple query, to find the authors who have viewed at least one of their own articles, we just need to select the author_ids where the author_id is equal to the viewer_id for each view, and order in ascending order */
