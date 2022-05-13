e2193b413ff5ba6c89fd35baceb3190d5bed5e09
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-05-13 10:58:07 +0100

    1407 solution

--- sql/1407-top-travellers.sql
+++ sql/1407-top-travellers.sql
@@ -1,1 +1,83 @@
 -- 1407 - Top Travellers
+
+/* Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key for this table.
+name is the name of the user.
+
+
+Table: Rides
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| user_id       | int     |
+| distance      | int     |
++---------------+---------+
+id is the primary key for this table.
+user_id is the id of the user who traveled the distance "distance".
+
+
+Write an SQL query to report the distance traveled by each user.
+
+Return the result table ordered by travelled_distance in descending order, if two or more users traveled the same distance, order them by their name in ascending order.
+
+Example 1:
+
+Input:
+Users table:
++------+-----------+
+| id   | name      |
++------+-----------+
+| 1    | Alice     |
+| 2    | Bob       |
+| 3    | Alex      |
+| 4    | Donald    |
+| 7    | Lee       |
+| 13   | Jonathan  |
+| 19   | Elvis     |
++------+-----------+
+Rides table:
++------+----------+----------+
+| id   | user_id  | distance |
++------+----------+----------+
+| 1    | 1        | 120      |
+| 2    | 2        | 317      |
+| 3    | 3        | 222      |
+| 4    | 7        | 100      |
+| 5    | 13       | 312      |
+| 6    | 19       | 50       |
+| 7    | 7        | 120      |
+| 8    | 19       | 400      |
+| 9    | 7        | 230      |
++------+----------+----------+
+Output:
++----------+--------------------+
+| name     | travelled_distance |
++----------+--------------------+
+| Elvis    | 450                |
+| Lee      | 450                |
+| Bob      | 317                |
+| Jonathan | 312                |
+| Alex     | 222                |
+| Alice    | 120                |
+| Donald   | 0                  |
++----------+--------------------+ */
+
+SELECT u.name, COALESCE(SUM(r.distance), 0) travelled_distance
+FROM Users u
+LEFT JOIN Rides r
+ON r.user_id = u.id
+GROUP BY u.name
+ORDER BY SUM(r.distance) DESC, u.name;
+
+/* SOLUTION - we need to join the two tables to link the user_id from the Rides table to the id of the Users table, which would allow us to do a sum of the distance travelled and group by the names of the users.
+
+In order to capture all the users, we need to do a left join to preserve any users without any distance travelled, and in order to remove the NULL value in the distance column, we need to use the COALESCE function to replace all occurrences of NULL with 0 instead. */
