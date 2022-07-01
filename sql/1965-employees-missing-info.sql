8a11bdce8ce1f2e0fda66dcb84e1646d50f6a39c
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-07-01 14:58:59 +0100

    1965 solution

--- sql/1965-employees-missing-info.sql
+++ sql/1965-employees-missing-info.sql
@@ -1,1 +1,77 @@
 -- 1965 - Employees with missing information
+
+/* Table: Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+
+
+Write an SQL query to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+The query result format is in the following example.
+
+
+
+Example 1:
+
+Input:
+Employees table:
++-------------+----------+
+| employee_id | name     |
++-------------+----------+
+| 2           | Crew     |
+| 4           | Haven    |
+| 5           | Kristian |
++-------------+----------+
+Salaries table:
++-------------+--------+
+| employee_id | salary |
++-------------+--------+
+| 5           | 76071  |
+| 1           | 22517  |
+| 4           | 63539  |
++-------------+--------+
+Output:
++-------------+
+| employee_id |
++-------------+
+| 1           |
+| 2           |
++-------------+
+Explanation:
+Employees 1, 2, 4, and 5 are working at this company.
+The name of employee 1 is missing.
+The salary of employee 2 is missing. */
+
+SELECT employee_id
+FROM Employees
+WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
+UNION
+SELECT employee_id
+FROM Salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;
+
+/* SOLUTION - you may think you need to do a full join for this, but there is an easier solution. You can simply use UNION and create two queries - the first selects all employee_ids from Employees that are not found in Salaries, and the second selects all employee_ids from Salaries that are not found in Employees. This lets us select all employee_ids that are not found in the other table from both tables. */
